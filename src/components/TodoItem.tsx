491c0638481fab0bd8401f023013085e55a59a90
cliske94 <cody.liske01@gmail.com>
2020-06-01 11:08:31 -0400

    finishing uploading files

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import { Space, Card, Checkbox } from 'antd';
+import Todo from "../services/Todo";
+
+interface TodoProp {
+    todo: Todo;
+}
+
+export default class TodoItem extends Component<TodoProp> {
+
+    constructor(props: TodoProp) {
+        super(props);
+    }
+
+    render() {
+        return (
+                <Card title={this.props.todo.title}>
+                    <p>Description</p>
+                    <p>{this.props.todo.description}</p>
+                    <p>Complete: <Checkbox defaultChecked={this.props.todo.complete} disabled /></p>
+                </Card>
+        );
+    }
+}
---
+++ src/components/todoList.tsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Todo from "../services/Todo";
+import {getTodos} from "../services/todoService";
+import {Button, Space} from 'antd';
+import TodoItem from "./TodoItem";
+
+interface TodoListState {
+  todos: Todo[];
+  loading: boolean;
+}
+class TodoList extends Component<any, TodoListState> {
+
+  state = {
+    todos: [],
+    loading: true
+  }
+
+  async componentDidMount() {
+    let todos = await getTodos();
+    this.setState({todos, loading: false});
+  }
+
+  render() {
+        return (
+            <div>
+              <h2>This is a todo list</h2>
+                { this.state.loading ?(
+                    <h3>TodoList is loading</h3>
+                    ) : (
+                        <>
+                            <Space direction="vertical" style={{width:300}}>
+                            {this.state.todos.map((todo: Todo) =>
+                                    <TodoItem key={todo.id} todo={todo}/>
+                            )}
+                            </Space>
+                        </>
+                    )}
+            </div>
+        );
+    }
+}
+
+export default TodoList;
